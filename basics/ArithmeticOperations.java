cee022ed15b781c0af497f1d52617e9969048baf
221HR075V-dot <221HR075V@s.internet.ac.jp>
2025-09-15 21:01:35 +0900

    Create ArithmeticOperations.java
    四則演算

---
+++ basics/ArithmeticOperations.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        int a = 12;
+        int b = 5;
+            System.out.println(a + b);
+            System.out.println(a - b);
+            System.out.println(a * b);
+            System.out.println(a / b);
+            System.out.println(a % b);
+    }
+ }
