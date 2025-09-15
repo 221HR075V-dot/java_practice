316e311bb347d2402608889c690f7c5add769091
221HR075V-dot <221HR075V@s.internet.ac.jp>
2025-09-15 18:09:40 +0900

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        int x = 7; //判定したい数字
+        
+        if (x % 2 == 0) {
+            System.out.println(x + "は偶数です");
+        } else { 
+            System.out.println(x + "は奇数です");
+        }
+    }
+}
