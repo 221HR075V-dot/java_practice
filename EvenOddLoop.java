bddb430c47240391b0d0ed5d136892e3c53edc62
221HR075V-dot <221HR075V@s.internet.ac.jp>
2025-09-15 20:34:55 +0900

    Create EvenOddLoop.java

---
+++ EvenOddLoop.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        for(int i = 1; i <= 20; i++){
+        
+            if (i % 2 == 0) {
+                System.out.println(i + "は偶数です");
+            } else {
+                System.out.println(i + "は奇数です");
+            }
+        }
+    }
+}
+
