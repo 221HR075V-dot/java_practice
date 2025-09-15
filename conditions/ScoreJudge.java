18f60c48459b3a894245b65c09858f9a0b9e0975
221HR075V-dot <221HR075V@s.internet.ac.jp>
2025-09-15 21:12:29 +0900

    Create ScoreJudge.java
    if / else if/ elseを使った点数判定

---
+++ conditions/ScoreJudge.java
@@ -0,0 +1,13 @@
+public class ScoreJudge {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        int score = 75;
+            if (score >= 80) {
+                System.out.println("優秀！");
+            } else if (score >= 60) {
+                System.out.println("合格");
+            } else {
+                System.out.println("不合格");
+            }
+        }
+    }
