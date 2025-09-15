239f6d00b8401e1c4a28dbad86cdb8308abab081
221HR075V-dot <221HR075V@s.internet.ac.jp>
2025-09-15 22:02:09 +0900

    Create DoWhileExample.java
    do-while文の簡単な演習

---
+++ loops/DoWhileExample.java
@@ -0,0 +1,10 @@
+public class DoWhileExample {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        int i = 10;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i<= 5);
+    }
+}
