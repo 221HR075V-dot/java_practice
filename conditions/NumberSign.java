756e86fea036efb216101632bd143fb9791c4f99
221HR075V-dot <221HR075V@s.internet.ac.jp>
2025-09-15 21:17:57 +0900

    Create NumberSign.java
    整数の正負判定

---
+++ conditions/NumberSign.java
@@ -0,0 +1,15 @@
+public class NumberSign {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        int n = -3;
+        
+        
+        if (n > 0) {
+            System.out.println(n + "は正の数です");
+        } else if (n < 0) {
+            System.out.println(n + "は負の数です");
+        } else {
+            System.out.println(n + "は0です");
+        }
+    }
+}
